6df1d468f7ddb6a2d4a43b6fa85e572d87408a46
lanleft <lanvuit201@gmail.com>
2021-03-03 16:34:00 +0700

    fix dlsym

--- 3_rootme/nanomites/files/ptrace_hook.c
+++ 3_rootme/nanomites/files/ptrace_hook.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <dlfcn.h>
-// #include <sys/ptrace.h>
+//#include <sys/ptrace.h>
 #include <sys/types.h>
 #include <stdarg.h>
 #include <sys/utsname.h>
@@ -15,13 +15,13 @@ void print_hex(const char *s, int idx)
     printf("\n");
 }
 
-long int (*orig_ptrace)(void* request, pid_t pid, void *addr, void *data);
+long int (*orig_ptrace) (int request, pid_t pid, void *addr, void *data);
 
-long ptrace(void* request, pid_t pid, void *addr, void *data)
+long int ptrace (int request, pid_t pid, void *addr, void *data)
 {
     if (!orig_ptrace) orig_ptrace = dlsym(RTLD_NEXT, "ptrace");
     unsigned int val = (unsigned int)request;
-    if ( val == 13){
+    if (val == 13){
         unsigned long rdi = *((unsigned long*)data + 15*8);
         printf("SETREGS: rdi: 0x%lx\n", rdi); 
         char* s = (char*)rdi;
@@ -30,5 +30,4 @@ long ptrace(void* request, pid_t pid, void *addr, void *data)
     return orig_ptrace(request, pid, addr, data);
 }
 
-
-// gcc -shared -fPIC -ldl ptrace_hook.c -w -o ptrace_hook.so && LD_PRELOAD=./ptrace_hook.so ./ch29.bin
+// gcc -shared -fPIC ptrace_hook.c -ldl -o ptrace_hook.so && LD_PRELOAD=./ptrace_hook.so ./ch29.bin
